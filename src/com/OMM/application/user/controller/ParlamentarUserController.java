ade216c80bb9f47581d107186c4b40a3731c9e5c
ViBorges <vbda123@gmail.com>
2013-10-15 13:35:44 -0300

    Alterações na servlet

--- src/com/OMM/application/user/controller/ParlamentarUserController.java
+++ src/com/OMM/application/user/controller/ParlamentarUserController.java
@@ -1,22 +1,20 @@
 package com.OMM.application.user.controller;
 
-import java.io.PrintWriter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import com.OMM.application.user.model.*;
-import com.OMM.application.user.dao.*;
+
 
 public class ParlamentarUserController {
 	
 	private static ParlamentarUserController instance;	
-	SocketUser socketUser;
 	Parlamentar parlamentar;
 	
 	
 	private ParlamentarUserController(){
 		
-		socketUser = new SocketUser();
 		parlamentar = new Parlamentar();
 		
 	}
@@ -31,16 +29,8 @@ public class ParlamentarUserController {
 		return instance;
 	}
 	
-	public void teste(){
-		System.out.println("Oii");
-		socketUser.testeeee();
-	}
 	
-	public void pesquisarParlamentarPorNome (String nome) {
-
-		socketUser.getSaida().print(nome);
-		socketUser.getSaida().flush();
-	}
-	
-
 }
+	
+
+
--- src/com/OMM/application/user/view/GuiMain.java
+++ src/com/OMM/application/user/view/GuiMain.java
@@ -1,92 +1,109 @@
 package com.OMM.application.user.view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.OMM.application.user.helper.JSONHelper;
+import com.OMM.application.user.model.Parlamentar;
+
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 public class GuiMain extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gui_main);
-		
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
-		Button btn = (Button)findViewById(R.id.button1);
+
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		Button btn = (Button) findViewById(R.id.button1);
 		final MediaPlayer mp = MediaPlayer.create(this, R.raw.click);
-        btn.setOnClickListener(new View.OnClickListener() {
+		btn.setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View v) {
-                    	
-                    	mp.start();                        
-                    	Intent mudarTela = new Intent(GuiMain.this, GuiParlamentar.class);
-            		    startActivity(mudarTela);
-            		    GuiMain.this.finish();
-                    }
-                });
-        
-        Button sobre = (Button)findViewById(R.id.button2);
-        final MediaPlayer mpl = MediaPlayer.create(this, R.raw.click);
-        sobre.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
-                    @Override
-                    public void onClick(View v) {
-                    	
-                    	mpl.start();
-                    	Intent mudarTela = new Intent(GuiMain.this, GuiSobre.class);
-            		    startActivity(mudarTela);
-            		    GuiMain.this.finish();
-                    }
-                });
-        
-        handleIntent(getIntent());
-    }
+				mp.start();
+				Intent mudarTela = new Intent(GuiMain.this,
+						GuiParlamentar.class);
+				startActivity(mudarTela);
+				GuiMain.this.finish();
+			}
+		});
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        
-        handleIntent(intent);
-    }
+		Button sobre = (Button) findViewById(R.id.button2);
+		final MediaPlayer mpl = MediaPlayer.create(this, R.raw.click);
+		sobre.setOnClickListener(new View.OnClickListener() {
 
-    private void handleIntent(Intent intent) {
+			@Override
+			public void onClick(View v) {
 
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            //use the query to search your data somehow
-        }
-        
+				mpl.start();
+				Intent mudarTela = new Intent(GuiMain.this, GuiSobre.class);
+				startActivity(mudarTela);
+				GuiMain.this.finish();
+			}
+		});
+		
+		
+		
+		handleIntent(getIntent());
 	}
-        
-	
-	
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+
+		handleIntent(intent);
+	}
+
+	private void handleIntent(Intent intent) {
+
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			// use the query to search your data somehow
+		}
+
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+
 		getMenuInflater().inflate(R.menu.options_menu, menu);
-		
-		SearchManager searchManager =
-		           (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-		    SearchView searchView =
-		            (SearchView) menu.findItem(R.id.search).getActionView();
-		    searchView.setSearchableInfo(
-		            searchManager.getSearchableInfo(getComponentName()));
 
-		    return true;
-		
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		SearchView searchView = (SearchView) menu.findItem(R.id.search)
+				.getActionView();
+		searchView.setSearchableInfo(searchManager
+				.getSearchableInfo(getComponentName()));
+
+		return true;
+
 	}
-
 }
-	
-
 
